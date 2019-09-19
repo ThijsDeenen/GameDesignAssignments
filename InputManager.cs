ed8a88a1323de17c95d53914e458a5bba169400d
ThijsDeenen <50613570+ThijsDeenen@users.noreply.github.com>
2019-09-19 09:16:07 +0200

    Add files via upload

---
+++ InputManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    private int timer = 10;
+    Touch touch;
+    Vector2 originalPosition;
+    Vector2 finalPosition;
+    float angle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Moved)
+            {
+                Vector2 direction = Input.touches[0].deltaPosition.normalized;
+                //originalPosition = touch.position;
+                cubePrefab.GetComponent<Movement>().ChangeDir(direction);
+            }
+           // if(touch.phase == TouchPhase.Ended)
+           // {
+            //    finalPosition = touch.position;
+            //    GetDirection(originalPosition, finalPosition);
+           // }
+        }
+    }
+
+    //void GetDirection(Vector2 start, Vector2 end)
+    //{
+    //    angle = Vector2.Angle(start, end);
+    //    cubePrefab.GetComponent<Movement>().ChangeDir(angle);
+    //}
+}
---
+++ Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Vector2 direction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime * 5);
+    }
+
+    public void ChangeDir(Vector2 angle)
+    {
+        //float radian = angle * Mathf.Rad2Deg;
+        direction = angle;
+    }
+}
